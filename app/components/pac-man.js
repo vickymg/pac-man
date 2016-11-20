d0a64f6a50e8a5e251bca7a18f3d1b30a2e150d3
Vicky Gray <vickygray26@hotmail.com>
2016-11-20 14:27:55 +0000

    Add keyboard shortcuts
    - Split x, y, squaresize properties out from drawCircle function

--- app/components/pac-man.js
+++ app/components/pac-man.js
@@ -1,21 +1,34 @@
 import Ember from 'ember';
+import KeyboardShortcuts from 'ember-keyboard-shortcuts/mixins/component';
 
-export default Ember.Component.extend({
+export default Ember.Component.extend(KeyboardShortcuts, {
+
   didInsertElement: function() {
     this.drawCircle();
   },
 
+  x: 50,
+  y: 100,
+  squareSize: 40,
+
   drawCircle: function() {
     let canvas = document.getElementById('myCanvas');
     let ctx = canvas.getContext('2d');
-    let x = 50;
-    let y = 100;
-    let radius = 20;
+    let x = this.get('x');
+    let y = this.get('y');
+    let radius = this.get('squareSize')/2;
 
     ctx.fillStyle = '#000';
     ctx.beginPath();
     ctx.arc(x, y, radius, 0, Math.PI * 2, false);
     ctx.closePath();
     ctx.fill();
+  },
+
+  keyboardShortcuts: {
+    up: function() { this.incrementProperty('y', -1 * this.get('squareSize')); this.drawCircle()},
+    down: function() { this.incrementProperty('y', this.get('squareSize')); this.drawCircle()},
+    left: function() { this.incrementProperty('x', -1 * this.get('squareSize')); this.drawCircle()},
+    right: function() { this.incrementProperty('x', this.get('squareSize')); this.drawCircle()},
   },
 });
