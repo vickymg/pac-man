fc6ed94bc76b51e1d73bacfc765312d5fcba7a4f
Vicky Gray <vickygray26@hotmail.com>
2016-11-20 14:47:00 +0000

    Refactor methods
    - extract movePacMan method
    - compute the canvas context as a property

--- app/components/pac-man.js
+++ app/components/pac-man.js
@@ -11,9 +11,21 @@ export default Ember.Component.extend(KeyboardShortcuts, {
   y: 100,
   squareSize: 40,
 
-  drawCircle: function() {
+  ctx: Ember.computed(function(){
     let canvas = document.getElementById('myCanvas');
     let ctx = canvas.getContext('2d');
+    return ctx;
+  }),
+
+  keyboardShortcuts: {
+    up: function() { this.movePacMan('y', -1 * this.get('squareSize'));},
+    down: function() { this.movePacMan('y', this.get('squareSize'));},
+    left: function() { this.movePacMan('x', -1 * this.get('squareSize'));},
+    right: function() { this.movePacMan('x', this.get('squareSize'));},
+  },
+
+  drawCircle: function() {
+    let ctx = this.get('ctx');
     let x = this.get('x');
     let y = this.get('y');
     let radius = this.get('squareSize')/2;
@@ -25,19 +37,17 @@ export default Ember.Component.extend(KeyboardShortcuts, {
     ctx.fill();
   },
 
-  keyboardShortcuts: {
-    up: function() { this.incrementProperty('y', -1 * this.get('squareSize')); this.clearScreen(), this.drawCircle()},
-    down: function() { this.incrementProperty('y', this.get('squareSize')); this.clearScreen(), this.drawCircle()},
-    left: function() { this.incrementProperty('x', -1 * this.get('squareSize')); this.clearScreen(), this.drawCircle()},
-    right: function() { this.incrementProperty('x', this.get('squareSize')); this.clearScreen(), this.drawCircle()},
-  },
-
   clearScreen: function() {
-    let canvas = document.getElementById('myCanvas');
-    let ctx = canvas.getContext('2d');
+    let ctx = this.get('ctx');
     let screenWidth = 800;
     let screenHeight = 600;
 
     ctx.clearRect(0, 0, screenWidth, screenHeight)
+  },
+
+  movePacMan: function(direction, amount) {
+    this.incrementProperty(direction, amount);
+    this.clearScreen();
+    this.drawCircle();
   }
 });
